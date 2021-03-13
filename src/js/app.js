ada7dcf98a51085b57c71b222e86cac1b5bd594e
Jhon Freiman Arias <jhonarias1322@gmail.com>
2021-03-13 10:28:32 -0500

    agregando paginacion y tabs con javascript y formulario, todo OK

--- src/js/app.js
+++ src/js/app.js
@@ -1,12 +1,71 @@
+//variables globales.. disponible a todas las funciones
+let pagina = 1;
+
 document.addEventListener('DOMContentLoaded', function() {
     iniciarApp();
     mostrarServicios();
-});
 
+});
+// funcion que inicia la App
 function iniciarApp() {
-    console.log('inciando App');
+    console.log('iniciando App...');
+
+    // resalta el div actual segun el div que se presiona
+    mostrarSeccion();
+
+    // oculta o muestra una seccion segun el tab alq ue se presiona  
+    cambiarSeccion();
+
+    paginaSiguiente();
+
+    paginaAnterior();
+
+    //comprueba la pagina actual para ocultar o mostrar
+    botonesPaginador();
+}
+
+
+function mostrarSeccion() {
+
+    // eliminar mostrar-seccion de la seccion antetior
+    const seccionAnterior = document.querySelector('.mostrar-seccion');
+    if (seccionAnterior) {
+        seccionAnterior.classList.remove('mostrar-seccion');
+    }
+
+    const seccionActual = document.querySelector(`#paso-${pagina}`);
+    seccionActual.classList.add('mostrar-seccion');
+
+    //elimina la clase de actual en el tab anterior
+    const tabAnterior = document.querySelector('.tabs .actual');
+    if (tabAnterior) {
+        tabAnterior.classList.remove('actual');
+    }
+
+    //resalta el tab actual
+    const tab = document.querySelector(`[data-paso="${pagina}"]`);
+    tab.classList.add('actual');
 
 }
+//funcion para seleccionar un tabs 
+function cambiarSeccion() {
+    const enlaces = document.querySelectorAll('.tabs button');
+
+    //itera cada elemento de enlaces
+    enlaces.forEach(enlace => {
+        enlace.addEventListener('click', e => {
+            e.preventDefault();
+            pagina = parseInt(e.target.dataset.paso);
+
+            //llamar la funcion de llamar seccion
+            mostrarSeccion();
+            botonesPaginador();
+        })
+    })
+
+
+}
+
 
 //funcion asincrona para consultas de datos de un archivo JSON
 async function mostrarServicios() {
@@ -35,12 +94,15 @@ async function mostrarServicios() {
             const servicioDiv = document.createElement('DIV'); // se crea un DIV
             servicioDiv.classList.add('servicio'); // se le agrega la clase servicio
 
+            // leer el dataset id y agregarlo en el servicioDiv
+            servicioDiv.dataset.idServicios = id;
+
+            //funcion para seleccionar el div con onclick
+            servicioDiv.onclick = seleccionarServicio;
+
             // Inyectar precio  y nombre junto al div de servicio
             servicioDiv.appendChild(nombreServicio); // se le pone como higjo nombreServicio
             servicioDiv.appendChild(precioServicio);
-
-            // selecionar un servicio para la cita 
-            servicioDiv.onclick = selecionarServicio;
 
 
 
@@ -61,23 +123,73 @@ async function mostrarServicios() {
         console.log('error..');
 
     }
+}
 
+// seleccionar servicios
+function seleccionarServicio(e) {
+    let elemento;
 
-    function selecionarServicio(e) {
-
-        let elemento;
-
-        //forzar que el elemento al cual le damos click sea el DIV
-        if (e.target.tagName === 'P') {
-            elemento = e.target.parentElement;
-        } else {
-            elemento = e.target;
-        }
-
-        if (elemento.classList.contains('seleccionado')) {
-            elemento.classList.remove('seleccionado');
-        } else {
-            elemento.classList.add('seleccionado');
-        }
+    // forzar a el elemento seleccionado para que tenga como tagName el div
+    if (e.target.tagName === "P") {
+        elemento = e.target.parentElement;
+    } else {
+        elemento = e.target;
     }
+
+    //agregar la clase seleccionado
+
+    if (elemento.classList.contains('seleccionado')) {
+        elemento.classList.remove('seleccionado');
+    } else {
+        elemento.classList.add('seleccionado');
+    }
+
+
+}
+
+// paginadores
+function paginaSiguiente() {
+
+    const paginaSiguiente = document.querySelector('#siguiente');
+    paginaSiguiente.addEventListener('click', () => {
+        pagina++;
+
+
+        botonesPaginador();
+        console.log(pagina);
+
+    })
+
+}
+
+function paginaAnterior() {
+    const paginaAnterior = document.querySelector('#anterior');
+    paginaAnterior.addEventListener('click', () => {
+        pagina--;
+
+        botonesPaginador();
+        console.log(pagina);
+
+    })
+
+}
+
+//comprobador de pagina actual
+function botonesPaginador() {
+    const paginaSiguiente = document.querySelector('#siguiente');
+    const paginaAnterior = document.querySelector('#anterior');
+
+    if (pagina === 1) {
+        paginaAnterior.classList.add('ocultar');
+        paginaSiguiente.classList.remove('ocultar');
+    } else if (pagina === 3) {
+        paginaSiguiente.classList.add('ocultar');
+        paginaAnterior.classList.remove('ocultar');
+    } else {
+        paginaAnterior.classList.remove('ocultar');
+        paginaSiguiente.classList.remove('ocultar');
+
+    }
+
+    mostrarSeccion();
 }
